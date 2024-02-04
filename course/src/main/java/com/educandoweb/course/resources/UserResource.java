36a94ea06c0be9f0f4f12da69b64c19909f71b3e
arturalexandree <arturalexandredemelo@gmail.com>
2024-02-04 14:27:33 -0300

    JPA repository, dependency injection, database seeding

--- course/src/main/java/com/educandoweb/course/resources/UserResource.java
+++ course/src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -12,7 +12,7 @@ public class UserResource {
 
     @GetMapping
     public ResponseEntity<User> findAll() {
-        User u = new User(1, "maria", "maria@gmail.com", "999999999", "12345");
+        User u = new User(null, "maria", "maria@gmail.com", "999999999", "12345");
         return ResponseEntity.ok().body(u);
     }
 }
