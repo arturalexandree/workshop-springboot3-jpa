5db53e688f51620441cede5e13eb276ae9ddd357
arturalexandree <arturalexandredemelo@gmail.com>
2024-02-11 10:02:08 -0300

    Product-OrderItem one-to-many association

--- course/src/main/java/com/educandoweb/course/entities/OrderItem.java
+++ course/src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -55,6 +55,7 @@ public class OrderItem implements Serializable {
         id.setOrder(order);
     }
 
+
     public Product getProduct() {
         return id.getProduct();
     }
--- course/src/main/java/com/educandoweb/course/entities/Product.java
+++ course/src/main/java/com/educandoweb/course/entities/Product.java
@@ -1,5 +1,6 @@
 package com.educandoweb.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -23,6 +24,10 @@ public class Product implements Serializable {
     @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name="product_id"),
     inverseJoinColumns = @JoinColumn(name="category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "id.product")
+    public Set<OrderItem> items = new HashSet<>();
 
     public Product() {
 
@@ -74,6 +79,15 @@ public class Product implements Serializable {
 
     public void setImgUrl(String imgUrl) {
         this.imgUrl = imgUrl;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+       Set<Order> set = new HashSet<>();
+       for(OrderItem x : items) {
+           set.add(x.getOrder());
+       }
+       return set;
     }
 
     public Set<Category> getCategories() {
